329cbe157dce6b0b8fca5815975b0ea219d294a8
alexghow903 <97628131+alexghow903@users.noreply.github.com>
2022-02-06 21:31:57 -0700

    Add files via upload

---
+++ Game/Jumper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace jumper
+{
+    public class chute
+    {
+        public void display(int track)
+        {
+            if (track == 0)
+            {
+                Console.WriteLine("  ___  ");
+                Console.WriteLine(" /___\\ ");
+                Console.WriteLine(" \\   / ");
+                Console.WriteLine("  \\ /  ");
+                Console.WriteLine("   0   ");
+                Console.WriteLine("  /|\\  ");
+                Console.WriteLine("  / \\  ");
+                Console.WriteLine("       ");
+                Console.WriteLine("^^^^^^^");
+                Console.WriteLine("       ");
+            }
+            else if (track == 1)
+            {
+                Console.WriteLine(" /___\\ ");
+                Console.WriteLine(" \\   / ");
+                Console.WriteLine("  \\ /  ");
+                Console.WriteLine("   0   ");
+                Console.WriteLine("  /|\\  ");
+                Console.WriteLine("  / \\  ");
+                Console.WriteLine("       ");
+                Console.WriteLine("^^^^^^^");
+                Console.WriteLine("       ");
+            }
+            else if (track == 2)
+            {
+                Console.WriteLine(" \\   / ");
+                Console.WriteLine("  \\ /  ");
+                Console.WriteLine("   0   ");
+                Console.WriteLine("  /|\\  ");
+                Console.WriteLine("  / \\  ");
+                Console.WriteLine("       ");
+                Console.WriteLine("^^^^^^^");
+                Console.WriteLine("       ");
+            }
+            else if (track == 3)
+            {
+                Console.WriteLine("  \\ /  ");
+                Console.WriteLine("   0   ");
+                Console.WriteLine("  /|\\  ");
+                Console.WriteLine("  / \\ ");
+                Console.WriteLine("       ");
+                Console.WriteLine("^^^^^^^");
+                Console.WriteLine("       ");
+            }
+            else if (track == 4)
+            {
+                Console.WriteLine("   X   ");
+                Console.WriteLine("  /|\\  ");
+                Console.WriteLine("  / \\  ");
+                Console.WriteLine("       ");
+                Console.WriteLine("^^^^^^^");
+                Console.WriteLine("       ");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int wrong;
+            Console.WriteLine("How many times have you guessed wrong?");
+            wrong = Convert.ToInt32(Console.ReadLine());
+            chute pw = new chute();
+            pw.display(wrong);
+        }
+    }
+}
