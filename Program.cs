9a6ff9c87eac1e0a43725e43d5cd658529d5d175
johnstennett24 <jstennett830@gmail.com>
2022-02-07 12:21:42 -0700

    added and worked on the terminal service class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using jumper.Game;
+
+
+namespace hilo
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            Dealer dealer = new Dealer();
+            dealer.StartGame();
+        }
+    }
+}
